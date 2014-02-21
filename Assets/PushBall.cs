db290bd78774f950bb1cf182f53aad50e0b6beef
svelle <sven.svelle@gmail.com>
2014-02-22 00:58:58 +0100

    Baseproject

---
+++ Assets/PushBall.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PushBall : MonoBehaviour {
+	public float forcey;
+	public float forcex;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void OnMouseDown() {
+		rigidbody2D.velocity = new Vector2(0.0f, forcey);
+		//RaycastHit2D hit;
+
+	}
+
+	// Update is called once per frame  
+	void Update() {
+
+	}
+}
---
+++ Assets/positionecho.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class positionecho : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnMouseDown() {
+		print(Input.mousePosition);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
