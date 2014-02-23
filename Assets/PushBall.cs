039f653415046a6e0dcf91b4eaaebf8ec9234fc4
Foxel <storkner@googlemail.com>
2014-02-23 18:49:47 +0100

    New ballpush method

--- Assets/PushBall.cs
+++ Assets/PushBall.cs
@@ -3,17 +3,20 @@ using System.Collections;
 using System;
 
 public class PushBall : MonoBehaviour {
-	public float forcey;
-	public float forcex;
+	public float ForceX;
+	public float ForceY;
+
 	// Use this for initialization
 	void Start () {
 
 	}
 
-	void OnMouseDown() {
-		rigidbody2D.velocity = new Vector2(0.0f, forcey);
-		//RaycastHit2D hit;
 
+	void OnMouseEnter () {
+		var mouse = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		float x = (float)Math.Sin(rigidbody2D.transform.position.x - mouse.x);
+		float y = (float)Math.Sin(rigidbody2D.transform.position.y - mouse.y);
+		rigidbody2D.velocity = new Vector2(x * ForceX,y * ForceY);
 	}
 
 	// Update is called once per frame  
