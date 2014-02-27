6ca6d0b658217ef0e49b3f6d4f844006040a2b1d
svelle <sven.svelle@gmail.com>
2014-02-27 17:46:16 +0100

    added files

---
+++ Assets/groundscript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class groundscript : MonoBehaviour {
+
+	public GameObject ball;
+	public GameObject losertext;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.name == "Ball")
+		{
+			Destroy(col.gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/hitcounterupdate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hitcounterupdate : MonoBehaviour {
+
+	public GameObject hitcounter;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		hitcounter.guiText.text = guiText.text;
+	}
+}
