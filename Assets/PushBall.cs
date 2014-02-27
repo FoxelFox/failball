a31e152897d76c4763c36fd8cbb49ceaeaa92478
svelle <sven.svelle@gmail.com>
2014-02-27 16:48:20 +0100

    added hitcounter for ballhits
    also added cloud background and ground texture
    as well as sound for ballhits

--- Assets/PushBall.cs
+++ Assets/PushBall.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System;
 
 public class PushBall : MonoBehaviour {
 	public float ForceX;
 	public float ForceY;
+	public GameObject hitcounter;
+	public GameObject hitsound;
 
 	// Use this for initialization
 	void Start () {
@@ -17,6 +19,9 @@ public class PushBall : MonoBehaviour {
 		float x = (float)Math.Sin(rigidbody2D.transform.position.x - mouse.x);
 		float y = (float)Math.Sin(rigidbody2D.transform.position.y - mouse.y);
 		rigidbody2D.velocity = new Vector2(x * ForceX,y * ForceY);
+		int hitcount = int.Parse(hitcounter.guiText.text) + 1;
+		hitcounter.guiText.text = hitcount.ToString();
+		hitsound.audio.Play();
 	}
 
 	// Update is called once per frame  
