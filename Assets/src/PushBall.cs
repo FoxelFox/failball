23693c6eca693038aedfed997bec07223d10d9ee
Foxel <storkner@googlemail.com>
2014-03-09 11:55:56 +0100

    grouped some scene items

--- Assets/src/PushBall.cs
+++ Assets/src/PushBall.cs
@@ -8,9 +8,15 @@ public class PushBall : MonoBehaviour {
 	public GameObject hitcounter;
 	public GameObject hitsound;
 
+	private DateTime clickTime;
+
 	// Use this for initialization
 	void Start () {
 
+	}
+
+	void OnMouseDown() {
+		clickTime = System.DateTime.Now;
 	}
 
 	/// <summary>
@@ -20,20 +26,25 @@ public class PushBall : MonoBehaviour {
 	/// 	- play a sound
 	/// </summary>
 	void OnMouseEnter () {
-		// convert mouseposition to worldcoordinates
-		var mouse = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 
-		// calculate the force vector and apply it to the velocity
-		float x = (float)Math.Sin(rigidbody2D.transform.position.x - mouse.x);
-		float y = (float)Math.Sin(rigidbody2D.transform.position.y - mouse.y);
-		rigidbody2D.velocity = new Vector2(x * ForceX,y * ForceY);
+		var cTime = System.DateTime.Now;
+		//if((cTime - clickTime).Milliseconds < 500) {
 
-		// set hit counter
-		int hitcount = int.Parse(hitcounter.guiText.text) + 1;
-		hitcounter.guiText.text = hitcount.ToString();
+			// convert mouseposition to worldcoordinates
+			var mouse = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 
-		// play a sound
-		hitsound.audio.Play();
+			// calculate the force vector and apply it to the velocity
+			float x = (float)Math.Sin(rigidbody2D.transform.position.x - mouse.x);
+			float y = (float)Math.Sin(rigidbody2D.transform.position.y - mouse.y);
+			rigidbody2D.velocity = new Vector2(x * ForceX,y * ForceY);
+
+			// set hit counter
+			int hitcount = int.Parse(hitcounter.guiText.text) + 1;
+			hitcounter.guiText.text = hitcount.ToString();
+
+			// play a sound
+			hitsound.audio.Play();
+		//}
 	}
 
 	// Update is called once per frame  
